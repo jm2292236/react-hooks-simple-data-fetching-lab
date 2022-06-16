bfd7ce1e8f3f0d7ee3edad3ad04c11aeea8fc74d
Jaime Montes de Oca <jaime0422@live.com>
2022-06-15 21:09:54 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [dogPicture, setDogPicture] = useState("");
+
+    useEffect(() => {
+        console.log("fetching data...");
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            console.log({data});
+            setDogPicture(data.message);
+        })
+    }, []);
+    
+    
+    return (
+        <div className="App">
+            {!dogPicture ? 
+                <p>Loading...</p> 
+            :
+                <img src={dogPicture} alt={"A Random Dog"}></img>
+            }
+        </div>
+    )
+}
+
+export default App;
